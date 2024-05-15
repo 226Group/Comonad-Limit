6b1179a6a23a917aa815d5319f016dad60fe8c05
226Group <kitgroup126@gmail.com>
2024-05-15 13:22:27 +0300

    Replit integration

--- replit.nix
+++ replit.nix
@@ -1,8 +1,8 @@
 { pkgs }: {
     deps = [
-        (pkgs.haskellPackages.ghcWithPackages (pkgs: [
+        #(pkgs.haskellPackages.ghcWithPackages (pkgs: [
             # Put your dependencies here!
-        ]))
-        pkgs.haskell-language-server
+        #]))
+        #pkgs.haskell-language-server
     ];
 }
